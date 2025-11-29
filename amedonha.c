f4807d2585d88783b4444cb18bc7804fde7647a9
Caio Lima <eusoucaioemanuel@gmail.com>
2025-11-29 15:33:42 -0300

    Esboço #004

--- amedonha.c
+++ amedonha.c
@@ -5,7 +5,7 @@
 
 int main (int narg, char * argv[]) {
     int N;
-    jogador lista_jogadores;
+    jogador * lista_jogadores;
     inicializar (&lista_jogadores);
 
     printf("*** JOGO AMEDONHA ***\n\n");
@@ -16,8 +16,10 @@ int main (int narg, char * argv[]) {
         char * nome[12];
         printf("Qual o nome do jogador %d? (No máximo 12 letras) ",i+1);
         scanf("%s",nome);
-        while (strlen(nome) > 12)   // Tratamento de erro: nome acima de 12 caracteres
+        while (strlen(nome) > 12){  // Tratamento de erro: nome acima de 12 caracteres
             printf("Qual o nome do jogador %d? (NO MÁXIMO 12 LETRAS) ",i+1);
+            scanf("%s",nome);
+        }
         inserir(&lista_jogadores,i,nome);
     }
 
@@ -29,26 +31,48 @@ int main (int narg, char * argv[]) {
     for (int i=0; i<5; i++) {   // Início do jogo
         
         // Sorteio da letra
-        int indice1 = sorteioLetra();
 
-        /* K será utilizado como parâmetro para saber se uma letra já foi sorteada.
-        O vetor de letras recebe K exatamente na posição da letra que for sorteada, 
-        o que indica que ela não pode mais ser escolhida.*/
-         
-        while ( strcmp(letras[indice1],"K") == 0 ) 
-            indice1 = sorteioLetra();   // Realiza novo sorteio de letra
-        printf("A letra desta rodada é %c",letras[indice1]);
-        letras[indice1] = "K";   // Anula a possibilidade da letra escolhida sair de novo.
+            int indice1 = sorteioLetra();
+
+            /* K será utilizado como parâmetro para saber se uma letra já foi sorteada.
+            O vetor de letras recebe K exatamente na posição da letra que for sorteada, 
+            o que indica que ela não pode mais ser escolhida.*/
+            
+            while ( strcmp(letras[indice1],"K") == 0 ) 
+                indice1 = sorteioLetra();   // Realiza novo sorteio de letra
+            printf("A letra desta rodada é %c",letras[indice1]);
+            letras[indice1] = "K";   // Anula a possibilidade da letra escolhida sair de novo.
 
         // Sorteio do tema
-        int indice2 = sorteioTemas();
 
-        /* A estratégia agora é usar o X para indicar que o tema já saiu */
+            int indice2 = sorteioTemas();
 
-        while ( strcmp(temas[indice2],"X") == 0 ) 
-            indice2 = sorteioTemas();  // Realiza novo sorteio de letra
-        printf("A letra desta rodada é %c",letras[indice1]);
-        letras[indice1] = "K";   // Anula a possibilidade da letra escolhida sair de novo.
+            /* A estratégia agora é usar o X para indicar que o tema já saiu */
+
+            while ( strcmp(temas[indice2],"X") == 0 ) 
+                indice2 = sorteioTemas();  // Realiza novo sorteio de tema
+            printf("A categoria desta rodada é %s",temas[indice2]);
+            temas[indice2] = "X";   // Anula a possibilidade do tema escolhido sair de novo.
+
+        //  Sorteio dos jogadores
+
+            /* Cria uma lista que recebe a lista de ordem de sorteio dos jogadores */
+            jogador *ordem = sorteioJogador(lista_jogadores,N);
+
+            /* Imprime a ordem de sorteio dos jogadores */
+            imprimirJogadores (ordem,1);
+
+        // Iniciando o jogo
+
+            char c;
+            printf("Tecle [Enter] para iniciar a rodada: ");
+            scanf("%c",&c);
+            while (c != '\n') {
+                printf("Tecle [Enter] para iniciar a rodada: ");
+                scanf("%c",&c);
+            }
+
+            system("clear");
 
 
         
