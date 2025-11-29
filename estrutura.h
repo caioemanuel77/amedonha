1cc1f2339d6a5b7a49fa092032d9b16a8b03e4d9
Caio Lima <eusoucaioemanuel@gmail.com>
2025-11-29 13:54:24 -0300

    Esboço #002 (Alterando o header também)

--- estrutura.h
+++ estrutura.h
@@ -9,6 +9,8 @@ void inserir(jogador** l, int id, char* nome);
 
 jogador* criarNode(int id, char* nome);
 
+int sorteio_letra ();
+
 jogador sorteio_jogador(jogador**lista);
 
 void liberarLista(jogador** no);
