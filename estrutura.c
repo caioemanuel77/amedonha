70bf35df73268304ecc3911c498d89f929c2e81b
carlexandre <carlos.0301.ca@gmail.com>
2025-11-29 14:24:04 -0300

    Melhora do gerenciamento de memória no copyNode e criação de funções de Sorteio

--- estrutura.c
+++ estrutura.c
@@ -10,7 +10,6 @@ void inicializar (jogador** l){
 void inserir(jogador** l, int id, char* nome){
     jogador* primeiro = *l;
     jogador* novo = criarNode(id, nome);
-
     *l = novo;
     novo->prox = primeiro;
 }
@@ -32,18 +31,85 @@ jogador* criarNode(int id, char* nome){
     return novo;
 }
 
-jogador sorteio_jogador(jogador**lista){
+jogador* find(jogador* lista, int id){
+    jogador* atual = lista;
 
+    while (atual != NULL && atual->id != id){
+        atual = atual->prox;
+    }
+
+    return atual;
 }
 
-int sorteio_letra(){
+jogador* copyNode(jogador* original) {
+    if (original == NULL) {
+        return NULL;
+    }
 
+    jogador* novo = (jogador*) malloc(sizeof(jogador));
+    if (novo == NULL) return NULL;
+
+    novo->id = original->id;
+    novo->pontos = original->pontos;
+
+    if (original->nome != NULL) {
+        novo->nome = (char*) malloc(strlen(original->nome) + 1);
+        strcpy(novo->nome, original->nome);
+    } else {
+        novo->nome = NULL;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (original->respostas[i] != NULL) {
+            novo->respostas[i] = (char*) malloc(strlen(original->respostas[i]) + 1);
+            strcpy(novo->respostas[i], original->respostas[i]);
+        } else {
+            novo->respostas[i] = NULL;
+        }
+    }
+
+    novo->prox = NULL;
+
+    return novo;
 }
 
-char* sorteio_temas(char* vetor[]){
+jogador* sorteioJogador(jogador*lista, int tamanho){
+    int vetor[tamanho], i;
+    jogador* sorteados;
+    jogador* adicionar;
+    jogador *anterior = NULL;
+    inicializar(&sorteados);
+
+    for(i = 0; i < tamanho; i++)
+        vetor[i] = 1;
     
+    for(i = 0; i < tamanho; i++){
+        int valor = rand() % tamanho;
+        while(vetor[valor] != 1){
+            valor = rand() % tamanho;
+        }
+        vetor[valor] = 0;
+
+        adicionar = copyNode(find(lista, valor));
+
+        if(anterior == NULL)
+            sorteados = adicionar;
+        else
+            anterior->prox = adicionar;
+        anterior = adicionar;
+    }
     
-    
+    return sorteados;
+}
+
+int sorteioLetra(){
+    int valor = rand() % 23;
+    return valor;
+}
+
+int sorteioTemas(){
+    int valor = rand() % 5;
+    return valor;
 }
 
 void liberarLista(jogador** no) {
@@ -58,4 +124,11 @@ void liberarLista(jogador** no) {
     }
     
     *no = NULL; 
+}
+
+void imprimirJogadores(jogador* no, int i){
+    if (no == NULL)
+        return;
+    printf("%i. %s", no->nome);
+    imprimirJogadores(no->prox, i+1);
 }
