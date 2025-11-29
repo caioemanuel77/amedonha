4821e15aac3464c4a3589e6f073ccd7f06fbb51e
carlexandre <carlos.0301.ca@gmail.com>
2025-11-29 14:26:53 -0300

    Atualização protótipos das funções

--- estrutura.h
+++ estrutura.h
@@ -1,14 +1,25 @@
 typedef struct node{
     int id;
     char* nome;
+    char* respostas[5];
     int pontos;
     struct node *prox;
-}jogador;
+} jogador;
 
 void inserir(jogador** l, int id, char* nome);
 
 jogador* criarNode(int id, char* nome);
 
-jogador sorteio_jogador(jogador**lista);
+jogador* find(jogador* lista, int id);
+
+jogador* copyNode(jogador* original);
+
+int sorteioLetra ();
+
+int sorteioTemas ();
+
+jogador* sorteioJogador(jogador*lista, int tamanho);
+
+void imprimirJogadores(jogador* no, int i);
 
 void liberarLista(jogador** no);
